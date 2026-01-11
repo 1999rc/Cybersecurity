5f3920fde89229398f00057aa550410a8a9a67d8
Raees chishty <mohommedraees@gmail.com>
2026-01-11 13:58:43 +0530

    Enhance monitor script with CPU and disk usage

--- src/monitor.sh
+++ src/monitor.sh
@@ -6,8 +6,14 @@ LOG_FILE="$LOG_DIR/system.log"
 mkdir -p "$LOG_DIR"
 
 echo "===== $(date) =====" >> "$LOG_FILE"
-echo "Uptime:" >> "$LOG_FILE"
-uptime >> "$LOG_FILE"
+echo "CPU Load:" >> "$LOG_FILE"
+uptime  | awk -F 'Load avrage:' '{ print $2}' >> "$LOG_FILE"
+
+echo "Disk Usage:" >> "$LOG_FILE"
+df -h / >> "$LOG_FILE"
+
+echo "-----------------------------------" >> "$LOG_FILE"
+
 
 echo "Memory:" >> "$LOG_FILE"
 free -h >> "$LOG_FILE"
@@ -16,3 +22,4 @@ echo "Top Processes:" >> "$LOG_FILE"
 ps -eo pid,comm,%cpu,%mem --soft=-%cpu | head -6 >> "$LOG_FILE"
 
 echo "" >> "$LOG_FILE"
+
